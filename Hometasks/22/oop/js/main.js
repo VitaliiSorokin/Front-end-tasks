7068b47f7348698df94876a642c1de7478ce22a1
VitaliiSorokin <x3meast@gmail.com>
2015-06-13 01:21:30 +0300

    todolist

--- Hometasks/22/oop/js/main.js
+++ Hometasks/22/oop/js/main.js
@@ -1,4 +1,4 @@
-function noMoneyNoHoney(){
+
     //TODO: Find out why the amount of honey in the hive is not increasing.
     function Bee(capacity, hive) {
         this.capacity = capacity;
@@ -20,6 +20,18 @@ function noMoneyNoHoney(){
     Bee.prototype.unloadPollen = function() {
         hive.addPollen(this.capacity);
     }
-}
-noMoneyNoHoney();
+
+
+var hive = new Hive;
+var bee = new Bee(10, hive);
+console.log(hive.getPollen());
+
+bee.unloadPollen();
+    console.log(hive.getPollen());
+    bee.unloadPollen();
+    console.log(hive.getPollen());
+
+
+
+//noMoneyNoHoney();
 
